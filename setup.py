4048ee436d28f24a44ecb9395296562218776d8a
Danny Aziz <daanyaal997@gmail.com>
2018-05-02 06:38:31 +0000

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='country_to_currency',
+      version='0.1',
+      description='Find out a countries currencies',
+      url='http://github.com/dannyaziz97/country_to_currency',
+      author='Danny Aziz',
+      author_email='hello@dannyaziz.com',
+      license='MIT',
+      packages=['country_to_currency'],
+      zip_safe=False)
